a8e4d1fb2d0517848e03f9dae39f0fab37176944
Alberto Valero <alberto.valero@bq.com>
2024-04-10 00:24:21 +0200

    add stat functions

--- mod.ts
+++ mod.ts
@@ -7,14 +7,12 @@
 
 /**
  * Export all the functions from the files in the module Stats.
- * Includes: average, variance, stdDev, median, mode, range.
  * @module Stats
  */
 export * as Stats from "./stats.ts";
 
 /**
  * Export all the functions from the files in the module.
- * Includes: max, min.
  * @module Maths
  */
-export { max, min } from "./maths.ts";
+export * from "./maths.ts";
