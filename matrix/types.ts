10095c97c4b88ec11db7ef2a714f2d17a2a0e4ce
Alberto Valero <alberto.valero@bq.com>
2024-04-10 13:21:00 +0200

    define matrix type

--- matrix/types.ts
+++ matrix/types.ts
@@ -1,3 +1,11 @@
+/**
+ * Matrix type
+ * @typedef {Object} Matrix
+ * @property {number[]} data - Matrix data
+ * @property {number} rows - Number of rows
+ * @property {number} cols - Number of columns
+ * @example const matrix: Matrix = { data: [1, 2, 3, 4], rows: 2, cols: 2 }
+ */
 export type Matrix = {
   data: number[];
   rows: number;
