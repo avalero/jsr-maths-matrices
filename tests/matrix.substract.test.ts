224b26b057d99463cca60e97125c8e215298e048
Alberto Valero <alberto.valero@bq.com>
2024-04-10 13:04:08 +0200

    remove substractMany

--- tests/matrix.substract.test.ts
+++ tests/matrix.substract.test.ts
@@ -1,6 +1,6 @@
 // substract.test.ts
 import { assertEquals, assertThrows } from "jsr:@std/assert";
-import { substract, substractMany } from "../matrix/substract.ts";
+import { substract } from "../matrix/substract.ts";
 
 Deno.test(
   "substract: should substract two matrices of the same dimensions",
@@ -21,31 +21,6 @@ Deno.test(
       () => substract(a, b),
       Error,
       "Matrices must have the same dimensions"
-    );
-  }
-);
-
-Deno.test(
-  "substractMany: should substract multiple matrices of the same dimensions",
-  () => {
-    const matrices = [
-      { data: [3, 3, 3, 3], rows: 2, cols: 2 },
-      { data: [1, 1, 1, 1], rows: 2, cols: 2 },
-      { data: [1, 1, 1, 1], rows: 2, cols: 2 },
-    ];
-    const expected = { data: [1, 1, 1, 1], rows: 2, cols: 2 };
-    assertEquals(substractMany(matrices), expected);
-  }
-);
-
-Deno.test(
-  "substractMany: should throw an error when less than two matrices are provided",
-  () => {
-    const matrices = [{ data: [1, 2, 3, 4], rows: 2, cols: 2 }];
-    assertThrows(
-      () => substractMany(matrices),
-      Error,
-      "At least two matrices are required"
     );
   }
 );
